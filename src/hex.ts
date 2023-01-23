3a40e4df907d8113a0fae0267a0ba2c5d1e95063
Aamir Jawaid <ajawaid191@gmail.com>
2023-01-23 08:13:34 -0800

    Add different rotation depending on even or odd row

--- src/hex.ts
+++ src/hex.ts
@@ -7,21 +7,36 @@ let dt = 0;
 let speed = 0.03;
 
 class Hex {
-  constructor(public sketch: p5, public position: p5.Vector) { }
+  phaseDelayStart: number;
+  constructor(public sketch: p5, public position: p5.Vector, public isRowEven: boolean) {
+    const distanceFromEnd = p5.Vector.dist(this.position, new p5.Vector((window.innerWidth - width * 1.5) / 2, (window.innerHeight - width * 1.5) / 2));
+
+    this.phaseDelayStart = distanceFromEnd / 500;
+  }
+
+  modifiedDt(dt: number) {
+    const end = dt + 2 * Math.PI;
+    if (dt < this.phaseDelayStart) {
+      return 0;
+    } else {
+      return dt - this.phaseDelayStart;
+    }
+  }
 
   draw(dt: number) {
     this.sketch.fill(255);
     this.sketch.push();
     this.sketch.translate(this.position.x, this.position.y);
 
-    const phase = linear(this.position.x, width * 1.5, window.innerWidth - width * 1.5, 0, Math.PI);
+    dt = this.modifiedDt(dt);
+
     const angle = dt % (2 * Math.PI);
-    const angleRotation = easeInOutCubic(angle, 0, 2 * Math.PI, 0, 2 * Math.PI);
+    const angleRotation = easeInOutCubic(angle, 0, 2 * Math.PI, 0, 2 * Math.PI) * (this.isRowEven ? 1 : -1);
 
     this.sketch.rotate(angleRotation)
     this.sketch.beginShape();
 
-    for (let angle = 0; angle <= 2 * Math.PI; angle += Math.PI / 3) {
+    for (let angle = 0; angle <= 2 * Math.PI; angle += Math.PI / 2) {
       const x = (Math.cos(angle) * width) / 2;
       const y = (Math.sin(angle) * width) / 2;
       this.sketch.vertex(x, y);
@@ -36,6 +51,7 @@ export default function (sketch: p5) {
   sketch.setup = () => {
     sketch.createCanvas(window.innerWidth, window.innerHeight);
 
+    let row = 0;
     for (
       let x = width * 1.5;
       x < window.innerWidth - width * 1.5;
@@ -46,8 +62,9 @@ export default function (sketch: p5) {
         y < window.innerHeight - width * 1.5;
         y += width * 1.5
       ) {
-        cubes.push(new Hex(sketch, new p5.Vector(x, y)));
+        cubes.push(new Hex(sketch, new p5.Vector(x, y), row % 2 === 0));
       }
+      row++
     }
   };
 
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,4 +1,4 @@
 import p5 from "p5";
-import sketch from "./cube";
+import sketch from "./hex";
 
 new p5(sketch);
